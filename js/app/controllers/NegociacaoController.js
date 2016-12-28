3cd5552c016245d47c416527a92b6be4b14930db
matheusquintaes <matheus.quintaes@gmail.com>
2016-12-28 15:57:37 -0200

    First commit

---
+++ js/app/controllers/NegociacaoController.js
@@ -0,0 +1,26 @@
+class NegociacaoController{
+
+    constructor(){
+
+        let $ = document.querySelector.bind(document);
+
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+
+    }
+    adiciona(event) {
+
+        event.preventDefault();
+        
+        let date = new Date(this._inputData.value.split('-'));
+        let negociacao = new Negociacao(
+            date,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negociacao);
+
+    }
+}
